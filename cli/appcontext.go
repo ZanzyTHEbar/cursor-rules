15a8ac46fa068929ce762707b6ea4117932a36ce
ZanzyTHEbar <zacariahheim@gmail.com>
2025-08-24 16:49:21 +0100

    chore(cli): introduce Logger interface and use in AppContext

--- cli/appcontext.go
+++ cli/appcontext.go
@@ -6,14 +6,20 @@ import (
 	"github.com/spf13/viper"
 )
 
+// Logger is a minimal logging interface used by AppContext.
+type Logger interface {
+	Printf(format string, v ...interface{})
+}
+
 // AppContext carries shared dependencies for command factories.
 type AppContext struct {
 	Viper  *viper.Viper
-	Logger *log.Logger
+	Logger Logger
 }
 
 // NewAppContext creates a default AppContext with provided logger and viper instance.
-func NewAppContext(v *viper.Viper, l *log.Logger) *AppContext {
+// Passing nil for the logger uses the standard library default logger.
+func NewAppContext(v *viper.Viper, l Logger) *AppContext {
 	if v == nil {
 		v = viper.New()
 	}
